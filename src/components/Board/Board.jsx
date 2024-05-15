8dbebbdf20eeed1da2e6d95f503baeb3795fbbef
j-pcgoncalves <joao_goncalves_7@hotmail.com>
2024-05-15 21:39:56 +0100

    style Board and BoardCell component

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,8 +1,21 @@
+import "./Board.css"
+
+import BoardCell from "../BoardCell/BoardCell";
+
 function Board({ board }) {
-    console.log("board", board);
+    const boardStyles = {
+        gridTemplateRows: `repeat(${board.size.rows}, 1fr)`,
+        gridTemplateColumns: `repeat(${board.size.columns}, 1fr)`
+    }
 
     return (
-        <p>Board</p>
+        <div className="Board" style={boardStyles}>
+            {board.rows.map((row, y) => 
+                row.map((cell, x) => (
+                    <BoardCell key={x * board.size.columns + y} cell={cell} />
+                ))
+            )}
+        </div>
     )
 }
 
