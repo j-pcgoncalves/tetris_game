5cda2fc7bb9332555c77553d10eaa089c91a1e30
j-pcgoncalves <joao_goncalves_7@hotmail.com>
2024-05-20 20:48:28 +0100

    create Previews component

--- src/components/Tetris/Tetris.jsx
+++ src/components/Tetris/Tetris.jsx
@@ -2,18 +2,22 @@ import "./Tetris.css"
 
 import Board from "../Board/Board";
 import GameStats from "../GameStats/GameStats";
+import Previews from "../Previews/Previews";
 
 import { useBoard } from "../../hooks/useBoard";
 import { useGameStats } from "../../hooks/useGameStats";
 
 function Tetris({ rows, columns, setGameOver }) {
     const [gameStats, addLinesCleared] = useGameStats();
-    const [board] = useBoard({ rows, columns })
+    const [board] = useBoard({ rows, columns });
+
+    const player = { tetrominoes: [] };
 
     return (
         <div className="Tetris">
             <Board board={board} />
             <GameStats gameStats={gameStats} />
+            <Previews tetrominoes={player.tetrominoes} />
         </div>
     )
 }
