c69f920964f9d3e9551d462b2653878f9a1bbece
j-pcgoncalves <joao_goncalves_7@hotmail.com>
2024-05-20 20:41:50 +0100

    create and style GameStats component, create useGameStats custom hook

--- src/components/Tetris/Tetris.jsx
+++ src/components/Tetris/Tetris.jsx
@@ -1,14 +1,20 @@
 import "./Tetris.css"
 
 import Board from "../Board/Board";
+import GameStats from "../GameStats/GameStats";
 
 import { useBoard } from "../../hooks/useBoard";
+import { useGameStats } from "../../hooks/useGameStats";
 
 function Tetris({ rows, columns, setGameOver }) {
+    const [gameStats, addLinesCleared] = useGameStats();
     const [board] = useBoard({ rows, columns })
 
     return (
-        <Board board={board} />
+        <div className="Tetris">
+            <Board board={board} />
+            <GameStats gameStats={gameStats} />
+        </div>
     )
 }
 
